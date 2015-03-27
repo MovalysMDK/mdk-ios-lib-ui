7e3a819d9404590b3a440dbbd4d5750bd14ebd54
Quentin <lagarde.quentin@sopragroup.com>
2015-03-27 14:15:44 +0100

    FIX : add tests

--- MFUITests/MFUITests.m
+++ MFUITests/MFUITests.m
@@ -1,40 +1,114 @@
+/**
+ * Copyright (C) 2010 Sopra (support_movalys@sopra.com)
+ *
+ * This file is part of Movalys MDK.
+ * Movalys MDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Movalys MDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Movalys MDK. If not, see <http://www.gnu.org/licenses/>.
+ */
 //
 //  MFUITests.m
 //  MFUITests
 //
-//  Created by Lagarde Quentin on 08/01/2015.
-//  Copyright (c) 2015 Lagarde Quentin. All rights reserved.
+//  Created by Laurent Michenaud on 10/10/2014.
+//  Copyright (c) 2014 Sopra Consulting. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+
+#import <MFCore/MFCoreFoundationExt.h>
+
+//Converters
+#import "MFStringConverter.h"
+#import "MFDateConverter.h"
+#import "MFNumberConverter.h"
+#import "MFHelperBOOL.h"
 
 @interface MFUITests : XCTestCase
 
 @end
 
+static NSString *const CONST_DATE_FORMAT = @"yyyy'-'MM'-'dd";
+
 @implementation MFUITests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testCreateList
+{
+    XCTAssertTrue(YES);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+
+-(void)testDateStringConverter {
+    NSString *dateString = @"2014-02-05";
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:CONST_DATE_FORMAT];
+    NSDate *convertedDate = [MFStringConverter toDate:dateString withFormatter:dateFormatter];
+    
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *convertedDateComponents = [gregorian components:(NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit) fromDate:convertedDate];
+    
+    XCTAssertEqual([convertedDateComponents day], 5);
+    XCTAssertEqual([convertedDateComponents month], 2);
+    XCTAssertEqual([convertedDateComponents year], 2014);
+    
+    NSString *newDateString = [MFDateConverter toString:convertedDate withCustomFormat:CONST_DATE_FORMAT];
+    
+    XCTAssertEqualObjects(dateString, newDateString);
 }
 
+-(void)testNumberStringConverter {
+    NSString *numberString = @"13.5";
+    NSNumber *convertedNumber = [MFStringConverter toNumber:numberString];
+    XCTAssertEqualObjects(convertedNumber, @(13.5));
+    
+    NSString *newNumberString = [MFNumberConverter toString:convertedNumber];
+    XCTAssertEqualObjects(numberString, newNumberString);
+}
+
+-(void)testBOOLHelper {
+    BOOL valid = YES;
+    BOOL invalid = NO;
+    
+    NSString *validString1 = @"YES";
+    NSString *validString2 = @"true";
+    NSString *validString3 = @"1";
+    
+    NSString *invalidString1 = @"NO";
+    NSString *invalidString2 = @"false";
+    NSString *invalidString3 = @"0";
+    
+    NSString *otherinvalid = @"Other";
+    
+    XCTAssertTrue([MFHelperBOOL booleanValue:validString1]);
+    XCTAssertTrue([MFHelperBOOL booleanValue:validString2]);
+    XCTAssertTrue([MFHelperBOOL booleanValue:validString3]);
+    
+    XCTAssertFalse([MFHelperBOOL booleanValue:invalidString1]);
+    XCTAssertFalse([MFHelperBOOL booleanValue:invalidString2]);
+    XCTAssertFalse([MFHelperBOOL booleanValue:invalidString3]);
+    XCTAssertFalse([MFHelperBOOL booleanValue:otherinvalid]);
+    
+    XCTAssertEqualObjects(validString1, [MFHelperBOOL asString:valid]);
+    XCTAssertEqualObjects(invalidString1, [MFHelperBOOL asString:invalid]);
+}
 @end

2367b5eef8a46eecd668ea6e0df0ddcdd72e08bf
Quentin <lagarde.quentin@sopragroup.com>
2015-04-29 10:28:28 +0200

    FIX : Copyrights

--- MFUI/control/button/MFUIControlButton.h
+++ MFUI/control/button/MFUIControlButton.h
@@ -1,10 +1,18 @@
-//
-//  MFUIControlButton.h
-//  MFUI
-//
-//  Created by Quentin Lagarde on 08/12/2014.
-//  Copyright (c) 2014 Sopra Consulting. All rights reserved.
-//
+/**
+ * Copyright (C) 2010 Sopra (support_movalys@sopra.com)
+ *
+ * This file is part of Movalys MDK.
+ * Movalys MDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Movalys MDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Movalys MDK. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef MFUI_MFUIControlButton_h
 #define MFUI_MFUIControlButton_h
